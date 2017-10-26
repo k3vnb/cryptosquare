80f130b9bfb1ce239a9870cc80eeb2eec25dc3bd
Kevin Boyle and Sam Kissee <kevin@>
2017-10-26 14:56:35 -0700

    add js to find out rows and collumns based on string lengths

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,22 @@
-var removePunc = function(input) {
-  var punc = ";:.,?!-'(){}";
-  var array = punc.split("");
-  for (i = 0; i < array.length; i++) {
-    if (input[i]=== array[i]){
-    input = input.replace(array[i], "");
-  } else {
-    return input;
-  }
-  }
+var toEncrypt = function(input){
+  var stringLength = input.length
+  var columns = Math.ceil(Math.sqrt(stringLength));
+  var rows = Math.round(Math.sqrt(stringLength));
+  var square = (columns * rows);
+  alert("columns" + columns);
+  alert("rows" + rows);
 }
-
-
-
-// var toEncrypt = function(input){
-//   var punc = ";:.,?!-'\"(){}";
-//   for (var i = 0; i < input.length; i++) {
-//     if (i === punc) {
-//
-//     }
-//   }
-//
-// }
-
-
-
 
 // Front End
 $(document).ready(function() {
   $("form#crypto-input").submit(function(event) {
     event.preventDefault();
     var userInput = $("input#original-message").val();
-    var lowerCaseInput = userInput.toLowerCase();
+    var re = /[\W_]/g;
+    var lowerCaseInput = userInput.toLowerCase().replace(re, '');
     var lcSplit = lowerCaseInput.split("");
-    var lcNoPunc = removePunc(lcSplit);
-    // var encryptedMessage = toEncrypt(lowerCaseInput);
-    $("#result").append("<li>" + lcNoPunc + "</li>");
+    var encryptedMessage = toEncrypt(lcSplit);
+    // $("#result").append("<li>" + lcSplit + "</li>");
 
   });
 });
