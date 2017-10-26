ebba4a0863445344a286720e3071b1bf58c82d2f
Kevin Boyle and Sam Kissee <sam@>
2017-10-26 11:46:27 -0700

    add js to split input into array, attempt to filter punctuation

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,40 @@
+var removePunc = function(input) {
+  var punc = ";:.,?!-'(){}";
+  var array = punc.split("");
+  for (i = 0; i < array.length; i++) {
+    if (input[i]=== array[i]){
+    input = input.replace(array[i], "");
+  } else {
+    return input;
+  }
+  }
+}
+
+
+
+// var toEncrypt = function(input){
+//   var punc = ";:.,?!-'\"(){}";
+//   for (var i = 0; i < input.length; i++) {
+//     if (i === punc) {
+//
+//     }
+//   }
+//
+// }
+
+
+
+
 // Front End
 $(document).ready(function() {
   $("form#crypto-input").submit(function(event) {
     event.preventDefault();
     var userInput = $("input#original-message").val();
     var lowerCaseInput = userInput.toLowerCase();
-    var encryptedMessage = toEncrypt(userInput);
-    $("#result").append("<li>" + encryptedMessage + "</li>")
+    var lcSplit = lowerCaseInput.split("");
+    var lcNoPunc = removePunc(lcSplit);
+    // var encryptedMessage = toEncrypt(lowerCaseInput);
+    $("#result").append("<li>" + lcNoPunc + "</li>");
 
   });
 });
